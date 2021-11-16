da0f7ea25bd973520f928c634bf802d5eb69204b
loperapablo <80335250+loperapablo@users.noreply.github.com>
2021-11-15 21:37:19 -0500

    Despliegue en heroku

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const app = express();
 //settings
 app.set('port', process.env.PORT || 3001);
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.header("Access-Control-Allow-Origin", "https://shrouded-shore-33122.herokuapp.com/");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
 
 const corsOptions = {
-   origin: 'http://localhost:3000',
+   origin: 'https://shrouded-shore-33122.herokuapp.com/',
    optionsSuccessStatus: 200,
    methods: "GET, PUT, POST, DELETE"
 }
--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ const onListening = () => {
   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
 };
 
-const port = normalizePort(process.env.PORT || "3001");
+const port = normalizePort('https://tranquil-beach-95447.herokuapp.com/' || "3001");
 app.set("port", port);
 
 const server = http.createServer(app);
