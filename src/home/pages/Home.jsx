8c406ef3d93bf4a1456fb4c1239359acb38d43e9
loperapablo <80335250+loperapablo@users.noreply.github.com>
2021-11-15 20:49:14 -0500

    Se agrega frontend

---
+++ src/home/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+import HABILISHOES from "./HABILISHOES.png"
+
+const Home = ({ isLoggedIn, carrito, setCarrito }) => {
+  return (
+    <div align="center">
+      <img src={HABILISHOES} width='100%' alt="Habilishoes"></img>
+    </div>
+  );
+};
+
+export default Home;
